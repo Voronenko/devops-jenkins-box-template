75cf4d8da1ff55d60889501b81372d0b8f507a6a
Vyacheslav Voronenko <git@voronenko.info>
2016-09-26 11:55:44 +0300

    bootstrap recipes for mac/pc

--- bootstrap_mac.sh
+++ bootstrap_mac.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+ 
 
-sudo apt-get install python-pip
-sudo pip install -U pip
+brew install git
+brew install python
+pip install -U pip
 sudo -H pip install ansible
 sudo -H pip install prudentia
